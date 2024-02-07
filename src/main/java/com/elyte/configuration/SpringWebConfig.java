dc8390826d4281eddc66a54627f0fb0073938ba1
elyte5star <checkuti@gmail.com>
2024-02-07 19:22:44 +0100

    cors

--- src/main/java/com/elyte/configuration/SpringWebConfig.java
+++ src/main/java/com/elyte/configuration/SpringWebConfig.java
@@ -70,7 +70,7 @@ public class SpringWebConfig implements WebMvcConfigurer, ApplicationContextAwar
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*").allowedOrigins("http://localhost:5173","http://localhost:9000","http://localhost:3000");
+        registry.addMapping("/*").allowedOrigins("http://localhost:9000","http://localhost:3000");
     }
 
     @Override
