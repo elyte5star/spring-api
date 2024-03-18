d767626033cecfe78d37e9b36ac81068283d4031
OGAGA UTI <elyte@OGAGAs-MacBook-Pro.local>
2024-03-18 15:05:54 +0100

    working on gmail auth

--- src/main/java/com/elyte/service/GmailValidation.java
+++ src/main/java/com/elyte/service/GmailValidation.java
@@ -2,13 +2,15 @@ package com.elyte.service;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.stereotype.Service;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 
+
+@Service
 public class GmailValidation {
 
     @Autowired
