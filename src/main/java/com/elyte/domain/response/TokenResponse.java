f925ef083afe5e2ae97c73ad2d1a14238f8a827c
elyte5star <checkuti@gmail.com>
2024-05-17 23:29:49 +0200

    modified jwt

--- src/main/java/com/elyte/domain/response/TokenResponse.java
+++ src/main/java/com/elyte/domain/response/TokenResponse.java
@@ -14,8 +14,8 @@ public class TokenResponse implements Serializable{
     @Serial
     private static final long serialVersionUID = -8191879091924046844L;
     
-    private String access_token;
-    private String token_type;
+    private String accessToken;
+    private String tokenType;
     private String username;
     private String email;
     private boolean enabled;
